545286dae93627bdaddcf89dbc9a08769fc55fcf
Shreya Sharma <vshlvani1234@gmail.com>
2023-05-25 09:51:49 +0000

    Thu, May 25, 2023, 9:51 AM +00:00

---
+++ lab.c
@@ -0,0 +1,28 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+    int n;
+    int m;
+    int t = 0;
+
+    do
+    {
+        n = get_int("Starting population : ");
+    }
+    while(n < 9);
+
+    do
+    {
+        m = get_int("Ending population : ");
+    }
+    while(m < n);
+
+    n = n + (n/3) - (n/4);
+        t++;
+
+    printf("Years: %i", t);
+
+}
