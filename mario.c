f6725a4e4221fb5e9916a151233248848922132c
Shreya Sharma <vshlvani1234@gmail.com>
2023-05-25 12:59:29 +0000

    Thu, May 25, 2023, 12:59 PM +00:00

--- mario.c
+++ mario.c
@@ -12,5 +12,12 @@ int main(void)
     }
     while (n < 1 || n > 8);
 
-    for
+    for(int i = 0 , i < n ; i++)
+    {
+        for(int j = 0 , j <= i , j++)
+        {
+            print("#");
+        }
+        printf("\n");
+    }
 }
