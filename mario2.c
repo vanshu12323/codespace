2c0a250f5cd9e5666bef9c05f9f0a2ab61d43152
Shreya Sharma <vshlvani1234@gmail.com>
2023-05-26 07:56:53 +0000

    Fri, May 26, 2023, 7:56 AM +00:00

--- mario2.c
+++ mario2.c
@@ -9,20 +9,24 @@ int main(void)
     int j;
     int k;
 
+    //GET INPUT
+
     do
     {
         n = get_int("Height: ");
     }
     while (n < 1 || n > 8);
 
-    for (i=0 ; i <= n ; i++)
+    //LOOP
+
+    for (i = 0 ; i <= n ; i++)
     {
-        for (k= i+1 ; k <= n ; k++)
+        for (k = i + 1 ; k <= n ; k++)
             {
                 printf(" ");
             }
 
-        for (j=0 ; j < i ; j++)
+        for (j = 0 ; j < i ; j++)
         {
             printf("#");
         }
