4436c719460d7486fd7e1a7aef5783922c9e89e4
Shreya Sharma <vshlvani1234@gmail.com>
2023-05-26 08:50:52 +0000

    Fri, May 26, 2023, 8:50 AM +00:00

--- mario.c
+++ mario.c
@@ -4,5 +4,42 @@
 
 int main(void)
 {
-    
+    int n;
+    int i;
+    int j;
+    int k;
+    int l;
+    int m;
+
+    //GET INPUT
+
+    do
+    {
+        n = get_int("Height: ");
+    }
+    while (n < 1 || n > 8);
+
+    //LOOP
+
+    for (i = 1 ; i <= n ; i++)
+    {
+        for (k = i + 1 ; k <= n ; k++)
+        {
+            printf(" ");
+        }
+        for (j = 0 ; j < i ; j++)
+        {
+            printf("#");
+        }
+        printf(" ");
+        for (j = 0 ; j < i ; j++)
+        {
+            printf("#");
+        }
+        for (m = i + 1 ; m <= n ; m++)
+        {
+            printf(" ");
+        }
+        printf("\n");
+    }
 }
