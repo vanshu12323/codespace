5e4474b5c6d1a127a1c26ea6309f5bb318592373
Shreya Sharma <vshlvani1234@gmail.com>
2023-05-27 10:39:15 +0000

    Sat, May 27, 2023, 10:39 AM +00:00

--- name.c
+++ name.c
@@ -2,13 +2,21 @@
 #include <stdio.h>
 #include <math.h>
 
+float average(int numbers[]);
+
 int main(void)
 {
-    int scores[3];
-    for (int i = 0 ; i < 3 ; i++)
-    {
-        scores[i] = get_int("Score: ");
-    }
+    scores =
 
     printf("Average: %f" , (scores[0] + scores[1] + scores[2]) / (float) 3);
+}
+
+float average(int numbers[])
+{
+    int score[];
+    for(int i = 0 ; i < 3 ; i++)
+    {
+        score[i] = get_int("Score: ");
+    }
+    printf("Average: %f" , score[i]/3.0);
 }
