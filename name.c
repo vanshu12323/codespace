62c1ec3bddfc272c1941c9b51705bf2fb2c399c0
Shreya Sharma <vshlvani1234@gmail.com>
2023-05-27 10:27:52 +0000

    Sat, May 27, 2023, 10:27 AM +00:00

--- name.c
+++ name.c
@@ -4,5 +4,9 @@
 
 int main(void)
 {
-    
+    int scores[3];
+    scores[0] = get_int("Score: ");
+    scores[1] = get_int("Score: ");
+    scores[2] = get_int("Score: ");
+    printf("Average: %f" , (scores[0] + scores[1] + scores[2]) / (float) 3);
 }
