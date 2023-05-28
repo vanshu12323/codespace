15c080939dc60a4f0d214fe445d8d21596e5202d
Shreya Sharma <vshlvani1234@gmail.com>
2023-05-28 06:53:30 +0000

    Sun, May 28, 2023, 6:53 AM +00:00

--- scrabble.c
+++ scrabble.c
@@ -14,13 +14,15 @@ int main(void)
     string aa = get_string("Player 1: ");
     string bb = get_string("Player 2: ");
 
-    int points[] = {1 == 'a' , 3 , 3, 2 , 1 , 4 , 2 , 4 , 1 , 8 , 5 , 1 , 3 , 1 , 1 , 3 , 10 , 1 , 1 , 1 , 1 , 4 , 4 , 8 , 4 , 10};
+    int points[] = {1 , 3 , 3, 2 , 1 , 4 , 2 , 4 , 1 , 8 , 5 , 1 , 3 , 1 , 1 , 3 , 10 , 1 , 1 , 1 , 1 , 4 , 4 , 8 , 4 , 10};
+
+    points[0] = 'a' , 'e';
 
     for (int i = 0 , n = strlen(aa) ; i < n ; i++)
     {
         if ((aa[i] >= 'a' && aa[i] <= 'z') || (aa[i] >= 'A' && aa[i] <= 'Z'))
         {
-            printf("%i " , aa[i]);
+            printf("%i " , points[0]);
         }
     }
 }
