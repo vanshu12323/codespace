aeb1ba6a011839542ee53f9d030967fbefee81e2
Shreya Sharma <vshlvani1234@gmail.com>
2023-05-28 07:29:39 +0000

    Sun, May 28, 2023, 7:29 AM +00:00

--- scrabble.c
+++ scrabble.c
@@ -39,15 +39,8 @@ int compute_score(string word)
     int points[] = {1 , 3 , 3, 2 , 1 , 4 , 2 , 4 , 1 , 8 , 5 , 1 , 3 , 1 , 1 , 3 , 10 , 1 , 1 , 1 , 1 , 4 , 4 , 8 , 4 , 10};
     int alphabets[] = {'a' , 'b' , 'c' , 'd' , 'e' , 'f' , 'g' , 'h' , 'i' , 'j' , 'k' , 'l' , 'm' , 'n' , 'o' , 'p' , 'q' , 'r' , 's' , 't' , 'u' , 'v' , 'w' , 'x' , 'y' , 'z'};
 
-    for(int i = 0 ; i < 26 ; i ++)
-    {
-        points[i] = alphabets[i];
-    }
 
 
-    for (int i = 0 , n = strlen(word) ; i < n ; i++)
-    {
-            word = tolower(word);
-            printf("%i" , points[word]);
-        }
+
+    
 }
