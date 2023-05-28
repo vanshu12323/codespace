c889cc7d1734678ac7a82781773bc28a1cbbf910
Shreya Sharma <vshlvani1234@gmail.com>
2023-05-28 06:04:32 +0000

    Sun, May 28, 2023, 6:04 AM +00:00

---
+++ scrabble.c
@@ -0,0 +1,18 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+int main(void)
+{
+    //TODO
+    //INPUT OF A AND B
+    //ASSIGN SCORE TO EACH ALPHABET
+    // COMPUTE SCORE AND BASED ON IT PRINT WHETHER A OR B WINS OR IS IT A TIE
+
+    string a = get_string("Player 1: ");
+    string b = get_string("Player 2: ");
+    
+
+
+}
