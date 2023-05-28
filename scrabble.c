c0765cd55f1f27b5897f3742bf98e5fc993b7951
Shreya Sharma <vshlvani1234@gmail.com>
2023-05-28 06:50:32 +0000

    Sun, May 28, 2023, 6:50 AM +00:00

--- scrabble.c
+++ scrabble.c
@@ -14,7 +14,7 @@ int main(void)
     string aa = get_string("Player 1: ");
     string bb = get_string("Player 2: ");
 
-    int points[] = {1 , 3 , 3, 2 , 1 , 4 , 2 , 4 , 1 , 8 , 5 , 1 , 3 , 1 , 1 , 3 , 10 , 1 , 1 , 1 , 1 , 4 , 4 , 8 , 4 , 10};
+    int points[] = {1 == 'a' , 3 , 3, 2 , 1 , 4 , 2 , 4 , 1 , 8 , 5 , 1 , 3 , 1 , 1 , 3 , 10 , 1 , 1 , 1 , 1 , 4 , 4 , 8 , 4 , 10};
 
     for (int i = 0 , n = strlen(aa) ; i < n ; i++)
     {
