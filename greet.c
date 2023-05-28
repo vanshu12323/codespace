ae77d2fab70bda2e6a35f858bc789c38708f4d0a
Shreya Sharma <vshlvani1234@gmail.com>
2023-05-28 04:13:36 +0000

    Sun, May 28, 2023, 4:13 AM +00:00

--- greet.c
+++ greet.c
@@ -7,5 +7,5 @@ int main(int argc , string argv[])
    /* string name = get_string("What's your name? ");
     printf("Hello , %s" , name); */
 
-    printf("Hello , %s" , argv[1]);
+    printf("Hello , %s" , argv[]);
 }
