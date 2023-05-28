e116100a04faffa599de16d6d1549b2b885b5359
Shreya Sharma <vshlvani1234@gmail.com>
2023-05-28 08:00:09 +0000

    Sun, May 28, 2023, 8:00 AM +00:00

--- scrabble.c
+++ scrabble.c
@@ -44,7 +44,13 @@ int compute_score(string word)
     for (int i = 0 ; i < strlen(word) ; i++)
     {
         char character = word[i];
-        
+        int m = 0;
+        if (character == alphabets[m])
+        {
+            m++;
+        }
         score += points[m];
     }
+
+    return score;
 }
