00b0811d41c7ea9f3c8a33007f6ecc92ba3de2ab
Shreya Sharma <vshlvani1234@gmail.com>
2023-05-28 07:32:51 +0000

    Sun, May 28, 2023, 7:32 AM +00:00

--- scrabble.c
+++ scrabble.c
@@ -37,10 +37,8 @@ int main(void)
 int compute_score(string word)
 {
     int points[] = {1 , 3 , 3, 2 , 1 , 4 , 2 , 4 , 1 , 8 , 5 , 1 , 3 , 1 , 1 , 3 , 10 , 1 , 1 , 1 , 1 , 4 , 4 , 8 , 4 , 10};
-    int alphabets[] = {'a' , 'b' , 'c' , 'd' , 'e' , 'f' , 'g' , 'h' , 'i' , 'j' , 'k' , 'l' , 'm' , 'n' , 'o' , 'p' , 'q' , 'r' , 's' , 't' , 'u' , 'v' , 'w' , 'x' , 'y' , 'z'};
-
-
-
+    char alphabets[] = {'a' , 'b' , 'c' , 'd' , 'e' , 'f' , 'g' , 'h' , 'i' , 'j' , 'k' , 'l' , 'm' , 'n' , 'o' , 'p' , 'q' , 'r' , 's' , 't' , 'u' , 'v' , 'w' , 'x' , 'y' , 'z'};
 
     
+
 }
