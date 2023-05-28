4883446585d9200dc23b376dd9269be446bc195c
Shreya Sharma <vshlvani1234@gmail.com>
2023-05-28 11:08:36 +0000

    Sun, May 28, 2023, 11:08 AM +00:00

--- scrabble.c
+++ scrabble.c
@@ -52,7 +52,7 @@ int compute_score(string word)
         char character = tolower(word[i]);
          int m = 0;
 
-        for (m = 0 ; character != alphabets[m] || (character >= alphabets[0] && character <= alphabets[25]) ; m++)
+        for (m = 0 ; character != alphabets[m] ; m++)
         {
             /*if (character >= alphabets[0] || character <= alphabets[25])
             {
