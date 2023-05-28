7a7aebc6c156fb2e01537579a6598d0db928b180
Shreya Sharma <vshlvani1234@gmail.com>
2023-05-28 09:45:43 +0000

    Sun, May 28, 2023, 9:45 AM +00:00

--- scrabble.c
+++ scrabble.c
@@ -52,7 +52,7 @@ int compute_score(string word)
         {
             if (character == alphabets[m])
             {
-                score += points[m];
+                score += 0;
             }
             else
             {
