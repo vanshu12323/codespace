d8b490eeee4cc933a3eaa6308bd67c85de4ac508
Shreya Sharma <vshlvani1234@gmail.com>
2023-05-29 06:50:50 +0000

    Mon, May 29, 2023, 6:50 AM +00:00

--- readability.c
+++ readability.c
@@ -4,18 +4,20 @@
 #include <string.h>
 #include <ctype.h>
 
+int count_letters(string text);
+
 int main(void)
 {
     string text = get_string("Text: ");
 
     int l = count_letters(text);
-    int w = count_words(text);
+    //int w = count_words(text);
     int s = count_sentences(text);
 
 
 }
 
-int count_letters(string word)
+int count_letters(string text)
 {
-    
+    return strlen(text);
 }
