81851cde2c685b3f0394ba2ab9382b0015773b6d
Shreya Sharma <vshlvani1234@gmail.com>
2023-05-30 09:45:03 +0000

    Tue, May 30, 2023, 9:45 AM +00:00

--- bulbs.c
+++ bulbs.c
@@ -36,12 +36,6 @@ int print_bulb(string message)
         }
     }
 
-    int remainder = 0;
-    while (n != 0)
-    {
-    remainder = n % 10;
-    reverse = reverse * 10 + remainder;
-    n /= 10;
-    }
+    
 
 }
