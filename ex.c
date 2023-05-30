c282fb94b1ad69fb74494239c992932e2274c91a
Shreya Sharma <vshlvani1234@gmail.com>
2023-05-30 11:13:22 +0000

    Tue, May 30, 2023, 11:13 AM +00:00

--- ex.c
+++ ex.c
@@ -25,7 +25,7 @@ int main(void)
 
    for (int i = 0 ; i < strnlen(s) ; i++)
    {
-      for (int j = 7 ; j >= 0 ; j--)
+      for (int j = i +  ; j >= 0 ; j--)
       {
          printf("%i" , remainder[]);
       }
