e7e97cb2bc7192736589ace4cd10faa08a9e6e4a
Shreya Sharma <vshlvani1234@gmail.com>
2023-05-30 10:07:18 +0000

    Tue, May 30, 2023, 10:07 AM +00:00

--- ex.c
+++ ex.c
@@ -8,12 +8,18 @@ int main(void)
 {
    string s = get_string("Message: ");
    int ascii[strlen(s)];
+   int remainder[];
 
    for (int i = 0 ; i < strlen(s) ; i++)
    {
       ascii[i] = s[i];
+
+      for (int i = 0 ; i < strlen(s) ; i++)
+      {
+         remainder[i] = ascii[i] / 2
+      }
    }
 
-   
+
 
 }
