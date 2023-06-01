c80e9c304779b30e1d0c2b3519d15efcd91b38b7
Shreya Sharma <vshlvani1234@gmail.com>
2023-06-01 14:23:26 +0000

    Thu, Jun 1, 2023, 2:23 PM +00:00

--- wordle/wordle.c
+++ wordle/wordle.c
@@ -99,6 +99,8 @@ int main(int argc, string argv[])
         // set all elements of status array initially to 0, aka WRONG
         // TODO #4
 
+        if ()
+
         // Calculate score for the guess
         int score = check_word(guess, wordsize, status, choice);
 
