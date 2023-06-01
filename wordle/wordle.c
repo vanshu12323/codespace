ee63cfb2d8156f4edfac2b491f5654a4293638ed
Shreya Sharma <vshlvani1234@gmail.com>
2023-06-01 14:06:28 +0000

    Thu, Jun 1, 2023, 2:06 PM +00:00

--- wordle/wordle.c
+++ wordle/wordle.c
@@ -50,7 +50,7 @@ int main(int argc, string argv[])
         }
     }
 
-    int wordsize = 0;
+    //int wordsize = 0;
 
     // ensure argv[1] is either 5, 6, 7, or 8 and store that value in wordsize instead
     // TODO #2
