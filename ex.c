b31b44e88c133364c4f76dd19d971148534e799c
Shreya Sharma <vshlvani1234@gmail.com>
2023-06-01 04:02:16 +0000

    Thu, Jun 1, 2023, 4:02 AM +00:00

--- ex.c
+++ ex.c
@@ -4,31 +4,7 @@
 #include <string.h>
 #include <ctype.h>
 
-int main(int argc , string argv[])
+int main(void)
 {
-    //int argv[1] = get_int("Key: ");
-
-    string text = get_string("plaintext:  ");
-
-    argv[1] = int key;
-
-    int s[strlen(text)];
-    string ct[strlen(text)];
-
-    printf("ciphertext: ");
-
-    for (int i = 0 ; i < strlen(text) ; i++)
-    {
-        if (text[i] == ' ')
-        {
-            printf(" ");
-        }
-        s[i] = text[i];
-        s[i] += key;
-        printf("%c", s[i]);
-    }
-
-
-
-
+    
 }
