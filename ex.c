6e510899cac3cfddc2c7e9c357d15baa89e240fc
Shreya Sharma <vshlvani1234@gmail.com>
2023-06-01 10:04:49 +0000

    Thu, Jun 1, 2023, 10:04 AM +00:00

--- ex.c
+++ ex.c
@@ -7,5 +7,17 @@
 
 int main(int argc, string argv[])
 {
-    
+    if (strlen(argv[1]) != 26)
+    {
+        printf("Key must contain 26 characters.")
+        return 1;
+    }
+
+    for (int i = 0 ; i < strlen(argv[1]) ; i++)
+    {
+        if (isalph(argv[1][i]) = false)
+        {
+            
+        }
+    }
 }
