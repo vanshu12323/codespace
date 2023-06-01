161e72600ee417f9f513995f1c9e09a65a815637
Shreya Sharma <vshlvani1234@gmail.com>
2023-06-01 05:11:31 +0000

    Thu, Jun 1, 2023, 5:11 AM +00:00

--- ex.c
+++ ex.c
@@ -6,5 +6,19 @@
 
 int main(void)
 {
-    
+    string text = get_string("plaintext:  ");
+    int m[strlen(text)];
+
+    for (int i = 0 ; i < strlen(text) ; i++)
+    {
+        if (isupper(text[i]))
+        {
+            m[i] = text[i];
+            printf("%i %c" , m , m);
+        }
+        else if(islower(text[i]))
+        {
+            m[i] = text[i];
+        }
+    }
 }
