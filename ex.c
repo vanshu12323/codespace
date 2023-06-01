df2a9b51a735ae359f20df614dcc8efd00587fef
Shreya Sharma <vshlvani1234@gmail.com>
2023-06-01 05:17:34 +0000

    Thu, Jun 1, 2023, 5:17 AM +00:00

--- ex.c
+++ ex.c
@@ -6,6 +6,7 @@
 
 int main(void)
 {
+    int key = get_int("Key: ");
     string text = get_string("plaintext:  ");
     int m[strlen(text)];
 
@@ -13,12 +14,31 @@ int main(void)
     {
         if (isupper(text[i]))
         {
-            m[i] = text[i];
-            printf("%i %c" , m , m);
+            m[i] = text[i] - 65;
         }
         else if(islower(text[i]))
         {
-            m[i] = text[i];
+            m[i] = text[i] - 97;
+        }
+    }
+
+    printf("ciphertext: ");
+
+    for (int i = 0 ; i < strlen(text) ; i++)
+    {
+        if (text[i] == ' ')
+        {
+            printf(" ");
+        }
+        else if (isupper(text[i]))
+        {
+            m[i] += key + 65;
+            printf("%c" , m[i]);
+        }
+        else if(islower(text[i]))
+        {
+            m[i] += key + 97;
+            printf("%c" , m[i]);
         }
     }
 }
