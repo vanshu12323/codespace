fb4961dba1aae7bf11b2b5c625e42a1c44c65d20
Shreya Sharma <vshlvani1234@gmail.com>
2023-06-02 08:50:02 +0000

    Fri, Jun 2, 2023, 8:50 AM +00:00

--- code.c
+++ code.c
@@ -7,5 +7,18 @@
 
 int main(void)
 {
-    
+    int numbers[] = {20, 500, 15, 5, 100, 1, 50};
+
+    int n = get_int("Numbers: ");
+
+    for (int i = 0; i < 7; i++)
+    {
+        if (numbers[i] == n)
+        {
+            printf("Found\n");
+            return 0;
+        }
+        printf("Not found\n");
+        return 1;
+    }
 }
