a31854f0792ff3afa22131916b0db2a9e3483763
Shreya Sharma <vshlvani1234@gmail.com>
2023-06-02 09:20:57 +0000

    Fri, Jun 2, 2023, 9:20 AM +00:00

--- code.c
+++ code.c
@@ -7,21 +7,5 @@
 
 int main(void)
 {
-    string names[] = {"Carter", "David"};
-    long numbers[] = {1234567890, 7890654321};
-
-    string name = get_string("Name: ");
-
-    for (int i = 0; i < 2; i++)
-    {
-        if (strcmp(names[i], name) == 0)
-        {
-            printf("%li", numbers[i]);
-            return 0;
-        }
-    }
-
-    printf("Not found");
-    return 1;
-
+    
 }
