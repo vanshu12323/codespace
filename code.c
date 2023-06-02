df423ed63d2ceca774cdb17c47471fc988cf25b0
Shreya Sharma <vshlvani1234@gmail.com>
2023-06-02 09:34:29 +0000

    Fri, Jun 2, 2023, 9:34 AM +00:00

--- code.c
+++ code.c
@@ -5,7 +5,35 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+typedef struct
+{
+    string name;
+    string number;
+}
+person;
+
 int main(void)
 {
-    
+    person people[2];
+
+    people[0].name = "Carter";
+    people[0].number = "1234567890";
+
+    people[1].name = "David";
+    people[1].number = "8234518009";
+
+    string name = get_string("Name: ");
+
+    for (int i = 0; i < 2; i++)
+    {
+        if (strcmp(names[i], name) == 0)
+        {
+            printf("%s", numbers[i]);
+            return 0;
+        }
+    }
+
+    printf("Not found");
+    return 1;
+
 }
