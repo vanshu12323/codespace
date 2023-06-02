dbb63f030eecc1aeaccf55667bd5279fa4bc3d9a
Shreya Sharma <vshlvani1234@gmail.com>
2023-06-02 16:42:10 +0000

    Fri, Jun 2, 2023, 4:42 PM +00:00

--- code.c
+++ code.c
@@ -17,15 +17,15 @@ int main(void)
 
 void draw(int n)
 {
-    if (n <= 0)
-    {
-        return;
-    }
-    draw(n - 1);
-
     for (int i = 0; i < n; i++)
     {
         printf("#");
     }
     printf("\n");
+
+    if (n <= 0)
+    {
+        return;
+    }
+    draw(n - 1);
 }
