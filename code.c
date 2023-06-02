5508106ed4e6375fd53fa504fec7608873011272
Shreya Sharma <vshlvani1234@gmail.com>
2023-06-02 06:25:53 +0000

    Fri, Jun 2, 2023, 6:25 AM +00:00

--- code.c
+++ code.c
@@ -5,7 +5,14 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-int main(int argc, string argv[])
+int main(void)
 {
-    
+    int array[5];
+    array[0] = 1;
+    printf("%i\n", array[0]);
+
+    for (int i = 1 ; i <= 4 ; i++)
+    {
+        printf("%i\n", array[i-1]*2);
+    }
 }
