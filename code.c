8a54e4f31825c2238f62b4ef72fe553ff35fdc13
Shreya Sharma <vshlvani1234@gmail.com>
2023-06-02 07:03:39 +0000

    Fri, Jun 2, 2023, 7:03 AM +00:00

--- code.c
+++ code.c
@@ -7,20 +7,5 @@
 
 int main(void)
 {
-    string text = get_string("Text: ");
-    int length = strlen(text);
-
-    for (int i = 1 ; i < length ; i++)
-    {
-            if ((int)text[i] < (int)text[i - 1])
-            {
-                printf("No");
-                return 0;
-            }
-            else
-            {
-                printf("Yes");
-                return 0;
-            }
-    }
+    
 }
