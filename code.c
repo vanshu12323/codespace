0bdd86f4a71a65cde375f91da3a77002221f15db
Shreya Sharma <vshlvani1234@gmail.com>
2023-06-02 06:33:37 +0000

    Fri, Jun 2, 2023, 6:33 AM +00:00

--- code.c
+++ code.c
@@ -7,12 +7,19 @@
 
 int main(void)
 {
-    int n = get_int("Length: ");
+    int n;
+
+    do
+    {
+        n = get_int("Length: ");
+    }
+    while (n <= 0);
+
     int array[n];
     array[0] = 1;
     printf("%i\n", array[0]);
 
-    for (int i = 1 ; i < 5 ; i++)
+    for (int i = 1 ; i < n ; i++)
     {
         array[i] = array[i-1]*2;
         printf("%i\n", array[i]);
