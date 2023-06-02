91c0d21eac4f503850ea81e3d5200873b3f48504
Shreya Sharma <vshlvani1234@gmail.com>
2023-06-02 11:53:28 +0000

    Fri, Jun 2, 2023, 11:53 AM +00:00

--- code.c
+++ code.c
@@ -5,35 +5,24 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-typedef struct
-{
-    string name;
-    string number;
-}
-person;
+void draw(int n);
 
 int main(void)
 {
-    person people[2];
+    int height = get_int("Height: ");
 
-    people[0].name = "Carter";
-    people[0].number = "1234567890";
+    draw(height);
+}
 
-    people[1].name = "David";
-    people[1].number = "8234518009";
-
-    string name = get_string("Name: ");
-
-    for (int i = 0; i < 2; i++)
+void draw(int n)
+{
+    for (int i = 0; i < n; i++)
     {
-        if (strcmp(people[i].name, name) == 0)
+        for (int j = 0; j <= i; j++)
         {
-            printf("%s", people[i].number);
-            return 0;
+            printf("#");
         }
+
+        printf("\n");
     }
-
-    printf("Not found");
-    return 1;
-
 }
