76cf7f1353dd1be01c08f209101017eebf75bd36
Shreya Sharma <vshlvani1234@gmail.com>
2023-06-03 14:04:07 +0000

    Sat, Jun 3, 2023, 2:04 PM +00:00

--- ex.c
+++ ex.c
@@ -70,7 +70,10 @@ int main(int argc, string argv[])
 
             for (int k = 0 ; strcmp(votes[i], candidates[k].name) != 0 ; k++)
             {
-                
+                if ((j == argc - 2) && (strcmp(votes[i], candidates[argc - 2].name) != 0))
+            {
+                printf("Invalid vote.\n");
+            }
             }
             /*else if ((j == argc - 2) && (strcmp(votes[i], candidates[argc - 2].name) != 0))
             {
