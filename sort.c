4c5d777c36107fc151a7d1bb2ae2013333120cd1
Shreya Sharma <vshlvani1234@gmail.com>
2023-06-04 08:44:04 +0000

    Sun, Jun 4, 2023, 8:44 AM +00:00

--- sort.c
+++ sort.c
@@ -11,5 +11,21 @@ int main(void)
     int sorted[7];
     int n[7] = {4, 5, 9, 2, 3, 0, 1};
 
-    
+    for (int i = 0 ; i < 3 ; i++)
+    {
+        if (n[i] > n[i+1])
+        {
+            n[i+1] = n[i];
+            n[i] = n[i+1];
+
+            for (int j = 0 ; j < 7 ; j++)
+            {
+                if (n[i] > n[i+1])
+                {
+                    n[i+1] = n[i];
+                    n[i] = n[i+1];
+                }
+            }
+        }
+    }
 }
