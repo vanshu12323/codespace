3f3b0587846d3183d60da4410916f671176c663a
Shreya Sharma <vshlvani1234@gmail.com>
2023-06-04 08:17:03 +0000

    Sun, Jun 4, 2023, 8:17 AM +00:00

--- sort.c
+++ sort.c
@@ -8,10 +8,25 @@
 
 int main(void)
 {
+    int sorted[7];
     int n[7] = {4, 5, 9, 2, 3, 0, 1};
 
     for (int i = 0 ; i < 7 ; i++)
     {
-        
+        for (int j = i + 1 ; j < 7 ; i++)
+        {
+            if (n[i] > n[j])
+            {
+                sorted[i] = n[j];
+                break;
+            }
+        }
+    }
+
+    printf("Sorted:\n");
+
+    for (int i = 0 ; i < 7 ; i++)
+    {
+        printf("%i\n", sorted[i]);
     }
 }
