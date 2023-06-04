61f668ad6f06b4f7d815d4ee61f94f03b55c2c98
Shreya Sharma <vshlvani1234@gmail.com>
2023-06-04 14:38:52 +0000

    Sun, Jun 4, 2023, 2:38 PM +00:00

--- plurality/m.c
+++ plurality/m.c
@@ -58,27 +58,7 @@ int main(int argc, string argv[])
             printf("Invalid vote.\n");
         }
     }
-
-    string name[argc - 1];
-
-    for (int i = 0 ; i < voter_count ; i++)
-    {
-        name[i] = get_string("Vote: ");
-
-        for (int j = 0 ; j < argc - 1 ; j++)
-        {
-            if (strcmp(votes[i], candidates[j].name) == 0)
-            {
-                candidates[j].vote += 1;
-                break;
-            }
-            else if (j == argc - 2 && strcmp(votes[i], candidates[j].name) != 0)
-            {
-                printf("Invalid vote.\n");
-            }
-        }
-    }
-
+    
     // Display winner of election
     print_winner();
 }
