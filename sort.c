20133b530064e3a028b7b48a719f5f136bd36e25
Shreya Sharma <vshlvani1234@gmail.com>
2023-06-04 12:30:05 +0000

    Sun, Jun 4, 2023, 12:30 PM +00:00

--- sort.c
+++ sort.c
@@ -11,10 +11,16 @@ int main(void)
     int sorted[7];
     int n[7] = {4, 5, 9, 2, 3, 0, 1};
 
-    (n[5] = n[4]) || (n[4] = n[5]);
+    for (int i = 0 ; i < 7 ; i++)
+    {
+        sorted[i] = n[i];
+    }
+
+    sorted[5] = n[4];
+    sorted[4] = n[5];
 
     for (int i = 0 ; i < 7 ; i++)
     {
-        printf("%i ", n[i]);
+        printf("%i ", sorted[i]);
     }
 }
