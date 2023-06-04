a0c234e28919a38ef40d2debd2d622d54c7b51fc
Shreya Sharma <vshlvani1234@gmail.com>
2023-06-04 07:22:46 +0000

    Sun, Jun 4, 2023, 7:22 AM +00:00

--- ex.c
+++ ex.c
@@ -79,20 +79,7 @@ int main(int argc, string argv[])
 
     for (int i = 0 ; i < argc - 1 ; i++)
     {
-        if (n % 2 == 0)
-        {
-            if (candidates[i].vote >= n/2)
-            {
-                printf("%s", candidates[i].name);
-            }
-        }
-        else if (n % 2 != 0)
-        {
-            if (candidates[i].vote >= (n +1)/2)
-            {
-                printf("%s", candidates[i].name);
-            }
-        }
+
     }
 }
 
