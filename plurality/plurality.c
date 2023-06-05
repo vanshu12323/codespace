5da028d06214314b335260da0418a079a1e1bbcb
Shreya Sharma <vshlvani1234@gmail.com>
2023-06-05 12:25:53 +0000

    Mon, Jun 5, 2023, 12:25 PM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -66,7 +66,8 @@ int main(int argc, string argv[])
 // Update vote totals given a new vote
 bool vote(string name)
 {
-    // TODO
+    int n = candidate_count;
+    string vote[n];
     return false;
 }
 
