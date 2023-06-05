6191e30d4b26dcb9817ea77ccdd686b319681661
Shreya Sharma <vshlvani1234@gmail.com>
2023-06-05 13:23:32 +0000

    Mon, Jun 5, 2023, 1:23 PM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -76,7 +76,7 @@ bool vote(string name)
         vote[i] = candidates[i].name;
     }
 
-    for (int i = 0 ; i < n ; i++)
+    for (int i = 0 ; i < m ; i++)
     {
         for (int j = 0 ; j < n ; j++)
         {
