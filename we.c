84d5b68e07404606ae4bbc50557c6ca7c6a24c67
Shreya Sharma <vshlvani1234@gmail.com>
2023-06-06 05:39:48 +0000

    Tue, Jun 6, 2023, 5:39 AM +00:00

--- we.c
+++ we.c
@@ -5,6 +5,9 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+// MAX. NO. OF CANDIDATES
+#define MAX 9
+
 typedef struct
 {
     string name;
@@ -14,28 +17,50 @@ typedef struct
 int candidate_count;
 int voter_count;
 
-candidate candidates[candidate_count];
+candidate candidates[MAX];
 
 int main(int argc, string argv[])
 {
     // CLA
-
-    int candidate_count = argc - 1; // candidate_count
+    candidate_count = argc - 1; // candidate_count
 
     if (candidate_count < 2)
     {
         printf("Usage: ./runoff [candidates...]");
     }
 
-    if (candidate_count > 9)
+    if (candidate_count > MAX)
     {
         printf("Maximum number of candidates can be 9 !");
     }
 
     // STORING THE NAME OF CANDIDATES IN AN ARRAY
-
     for (int i = 0; i < candidate_count; i++)
     {
         candidates[i].name = argv[i];
+    }
+
+    // Number of voters
+    voter_count = get_int("Number of voters: ");
+
+    // UPDATING NO. OF VOTES AND RANK OF EACH CANDIDATE
+    string votes[voter_count][candidate_count];
+
+    for (int i = 0; i < voter_count; i++)
+    {
+        votes[i] = get_string("Vote: ");
+
+        for (int j = 0; j < candidate_count; j++)
+        {
+            if (strcmp(votes[i], candidates[j].name) == 0)
+            {
+                candidates[j].vote += 1;
+                break;
+            }
+            else if (j == argc - 2 && strcmp(votes[i], candidates[j].name) != 0)
+            {
+                printf("Invalid vote.\n");
+            }
+        }
     }
 }
