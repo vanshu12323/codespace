893b8cec839b2fdcf621dd444896da7bc7c58d18
Shreya Sharma <vshlvani1234@gmail.com>
2023-06-06 10:44:56 +0000

    Tue, Jun 6, 2023, 10:44 AM +00:00

--- runofff/hehe.c
+++ runofff/hehe.c
@@ -1,5 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 // Max voters and candidates
 #define MAX_VOTERS 100
@@ -132,7 +133,7 @@ bool vote(int voter, int rank, string name)
 
     for(int i = 0; i < candidate_count; i++)
     {
-        if (strcmp(name, candidate[i].name) == 0)
+        if (strcmp(name, candidates[i].name) == 0)
         {
             present_or_not = true;
         }
@@ -141,6 +142,7 @@ bool vote(int voter, int rank, string name)
             present_or_not = false;
         }
     }
+    return present_or_not;
 }
 
 // Tabulate votes for non-eliminated candidates
