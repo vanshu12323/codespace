8b7a31ecfd569b880790ef0518dd84cd64b90d63
Shreya Sharma <vshlvani1234@gmail.com>
2023-06-06 14:04:04 +0000

    Tue, Jun 6, 2023, 2:04 PM +00:00

--- ss.c
+++ ss.c
@@ -2,7 +2,53 @@
 #include <stdio.h>
 #include <string.h>
 
+#define MAX_VOTERS 100
+#define MAX_CANDIDATES 9
+
+typedef struct
+{
+    string name;
+    int votes;
+    bool eliminated;
+}
+candidate;
+
+int voter_count;
+int candidate_count;
+
+candidate candidates[MAX_CANDIDATES];
+
 int main(int argc, string argv[])
 {
-    
+    // CLA
+    if (argc < 2)
+    {
+        printf("Usage: runoff [candidate ...]\n");
+        return 1;
+    }
+
+    // MAX NO. OF CANDIDATES
+    candidate_count = argc - 1;
+    if (candidate_count > MAX_CANDIDATES)
+    {
+        printf("Maximum number of candidates is %i\n", MAX_CANDIDATES);
+        return 2;
+    }
+
+    for (int i = 0; i < candidate_count; i++)
+    {
+        candidates[i].name = argv[i + 1];
+        candidates[i].votes = 0;
+        candidates[i].eliminated = false;
+    }
+
+    // MAX NO. OF VOTERS
+    voter_count = get_int("Number of voters: ");
+    if (voter_count > MAX_VOTERS)
+    {
+        printf("Maximum number of voters is %i\n", MAX_VOTERS);
+        return 3;
+    }
+
+
 }
