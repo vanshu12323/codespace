8caf203ae7815ff3bea072065ecf24b38af28f15
Shreya Sharma <vshlvani1234@gmail.com>
2023-06-06 05:33:47 +0000

    Tue, Jun 6, 2023, 5:33 AM +00:00

--- we.c
+++ we.c
@@ -11,12 +11,31 @@ typedef struct
     int votes;
 } candidate;
 
+int candidate_count;
+int voter_count;
+
+candidate candidates[candidate_count];
+
 int main(int argc, string argv[])
 {
-    int n = argc - 1; // candidate_count
+    // CLA
 
-    if (n < 2)
+    int candidate_count = argc - 1; // candidate_count
+
+    if (candidate_count < 2)
     {
         printf("Usage: ./runoff [candidates...]");
+    }
+
+    if (candidate_count > 9)
+    {
+        printf("Maximum number of candidates can be 9 !");
+    }
+
+    // STORING THE NAME OF CANDIDATES IN AN ARRAY
+
+    for (int i = 0; i < candidate_count; i++)
+    {
+        candidates[i].name = argv[i];
     }
 }
