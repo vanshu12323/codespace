6e7545c1e0e173bc306bf45830c5d2b4ee7fb197
Shreya Sharma <vshlvani1234@gmail.com>
2023-06-07 18:05:49 +0000

    Wed, Jun 7, 2023, 6:05 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -220,7 +220,7 @@ bool print_winner(void)
 
     for (int i = 0; i < candidate_count; i++)
     {
-        if (candidates[i].votes >= max_votes / 2.0)
+        if (candidates[i].votes > max_votes / 2.0)
         {
             printf("WINNER: %s\n", candidates[i].name);
             won = true;
