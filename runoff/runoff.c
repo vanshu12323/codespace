29b3c5a2a61267eaac8685c91f602a570cfc4a7f
Shreya Sharma <vshlvani1234@gmail.com>
2023-06-07 13:58:31 +0000

    Wed, Jun 7, 2023, 1:58 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -168,7 +168,10 @@ void tabulate(void)
 {
     for (int i = 0; i < voter_count; i++)
     {
-        for (int j = 0; j < )
+        for (int j = 0; j < candidate_count; j++)
+        {
+            
+        }
     }
 }
 
