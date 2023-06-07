3a6007bbe3817169bdc7cb5230f6929700615d8d
Shreya Sharma <vshlvani1234@gmail.com>
2023-06-07 15:17:37 +0000

    Wed, Jun 7, 2023, 3:17 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -216,13 +216,7 @@ bool print_winner(void)
 // Return the minimum number of votes any remaining candidate has
 int find_min(void)
 {
-    for (int i = 0; i < voter_count; i++)
-    {
-        for (int j = 0; j < candidate_count; j++)
-        {
-            
-        }
-    }
+    return 0;
 }
 
 
