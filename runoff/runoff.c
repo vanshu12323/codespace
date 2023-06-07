f17e2f92d10da6adca4ff6dcc4d3146112302e4a
Shreya Sharma <vshlvani1234@gmail.com>
2023-06-07 13:10:35 +0000

    Wed, Jun 7, 2023, 1:10 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -1,5 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 // Max voters and candidates
 #define MAX_VOTERS 100
@@ -141,7 +142,7 @@ bool vote(int voter, int rank, string name)
     {
         for (int j = 0; j < candidate_count; j++)
         {
-            if (strcmp(name, candidates[preference[i][j]].name) == 0)
+            if (strcmp(name, candidates[preferences[i][j]].name) == 0)
             {
                 present = true;
                 break;
