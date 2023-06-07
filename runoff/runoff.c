662c8a4ecd22870ab1113bce759110bc8563ae06
Shreya Sharma <vshlvani1234@gmail.com>
2023-06-07 15:22:30 +0000

    Wed, Jun 7, 2023, 3:22 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -202,7 +202,10 @@ void tabulate(void)
 // Print the winner of the election, if there is one
 bool print_winner(void)
 {
-    
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes)
+    }
 }
 
 
