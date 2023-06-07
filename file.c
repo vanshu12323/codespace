4aadc5fba0048298d54a7377358ca5264c55061d
Shreya Sharma <vshlvani1234@gmail.com>
2023-06-07 12:17:45 +0000

    Wed, Jun 7, 2023, 12:17 PM +00:00

--- file.c
+++ file.c
@@ -1,13 +1,9 @@
 #include <cs50.h>
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
-#include <ctype.h>
-
-void draw(int n);
 
 int main(void)
 {
-    
+
 }
