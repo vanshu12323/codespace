f727739b89bc51c60ed83f5340adf4ae9a78bf21
Shreya Sharma <vshlvani1234@gmail.com>
2023-06-07 05:29:03 +0000

    Wed, Jun 7, 2023, 5:29 AM +00:00

--- ss.c
+++ ss.c
@@ -137,10 +137,10 @@ int main(int argc, string argv[])
         eliminate(min);
 
         //Reset vote counts back to zero
-        for (int i = 0; i < candidate_count; i++)
-        {
-            candidates[i].votes = 0;
-        }
+        // for (int i = 0; i < candidate_count; i++)
+        // {
+        //     candidates[i].votes = 0;
+        // }
     }
     return 0;
 }
